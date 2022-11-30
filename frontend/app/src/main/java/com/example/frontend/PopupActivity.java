c4a8f90d54188951608f2a2b95f3e4738a78898e
subin <bin9841@gmail.com>
2022-12-01 03:12:29 +0900

    Modify Socket Communication

--- frontend/app/src/main/java/com/example/frontend/PopupActivity.java
+++ frontend/app/src/main/java/com/example/frontend/PopupActivity.java
@@ -1,11 +1,7 @@
 package com.example.frontend;
 
-import android.app.Activity;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.AsyncTask;
+
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
@@ -13,27 +9,12 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class PopupActivity extends AppCompatActivity {
     ImageView image;
     TextView alert_title, alert_content;
     Button btn_rsc, btn_rsc_cpt;
-    static String strJson ="";
     Person person;
 
     @Override
@@ -54,8 +35,6 @@ public class PopupActivity extends AppCompatActivity {
         btn_rsc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // HttpAsyncTask httpTask = new HttpAsyncTask(PopupActivity.this);
-                // httpTask.execute("http://localhost:8000/models/");
                 btn_rsc.setEnabled(false);
                 btn_rsc_cpt.setEnabled(true);
                 alert_title.setText("구조 중");
@@ -84,110 +63,7 @@ public class PopupActivity extends AppCompatActivity {
         return;
     }
 
-    public static String POST(String url, Person person){
-        InputStream is = null;
-        String result = "";
-        try {
-            URL urlCon = new URL(url);
-            HttpURLConnection httpCon = (HttpURLConnection)urlCon.openConnection();
 
-            String json = "";
-
-            // build jsonObject
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.accumulate("name", person.getName());
-
-            // convert JSONObject to JSON to String
-            json = jsonObject.toString();
-
-            // ** Alternative way to convert Person object to JSON string usin Jackson Lib
-            // ObjectMapper mapper = new ObjectMapper();
-            // json = mapper.writeValueAsString(person);
-
-            // Set some headers to inform server about the type of the content
-            httpCon.setRequestProperty("Accept", "application/json");
-            httpCon.setRequestProperty("Content-type", "application/json");
-
-            // OutputStream으로 POST 데이터를 넘겨주겠다는 옵션.
-            httpCon.setDoOutput(true);
-            // InputStream으로 서버로 부터 응답을 받겠다는 옵션.
-            httpCon.setDoInput(true);
-
-            OutputStream os = httpCon.getOutputStream();
-            os.write(json.getBytes("euc-kr"));
-            os.flush();
-            // receive response as inputStream
-            try {
-                is = httpCon.getInputStream();
-                // convert inputstream to string
-                if(is != null)
-                    result = convertInputStreamToString(is);
-                else
-                    result = "Did not work!";
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-            finally {
-                httpCon.disconnect();
-            }
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        catch (Exception e) {
-            Log.d("InputStream", e.getLocalizedMessage());
-        }
-
-        return result;
-    }
-
-    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
-
-        private   PopupActivity popAct;
-
-        HttpAsyncTask(PopupActivity popupActivity) {
-            this.popAct = popupActivity;
-        }
-        @Override
-        protected String doInBackground(String... urls) {
-
-            person = new Person();
-            person.setName(urls[1]);
-
-            return POST(urls[0],person);
-        }
-        // onPostExecute displays the results of the AsyncTask.
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            strJson = result;
-            popAct.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    Toast.makeText(popAct, "Received!", Toast.LENGTH_LONG).show();
-                    try {
-                        JSONArray json = new JSONArray(strJson);
-                        popAct.alert_title.setText("제목");
-                        popAct.alert_content.setText("내용");
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-
-        }
-    }
-
-    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
-            BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
-            String line = "";
-            String result = "";
-            while((line = bufferedReader.readLine()) != null)
-                result += line;
-            inputStream.close();
-            return result;
-        }
 
 
 }
